6f5fb9f4ce09571cc1be731bbe7310673443634e
francisco.molina <francisco.molina@enap.gov.br>
2017-06-21 10:43:13 -0300

    capitulo03-aula04 v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,27 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  pages: Array<{title: string, component: any, icon: string}> = [];
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    this.pages = [
+      {title: 'Home', component: HomePage, icon: 'home'},
+      {title: 'Overlay Components', component: HomePage, icon: ''},
+      {title: 'Components', component: HomePage, icon: ''}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page: any) : void {
+    this.rootPage = page.component;
   }
 }
 
